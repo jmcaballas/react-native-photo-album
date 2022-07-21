389a75e03f909f74549bf07e996966ed2a0fab97
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-07-22 03:11:17 +0800

    Add icon in Home screen to navigate to Settings screen

--- App.tsx
+++ App.tsx
@@ -2,9 +2,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ApplicationProvider } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { HomeScreen } from "./components/HomeScreen";
 import { PhotoDetailScreen } from "./components/PhotoDetailScreen";
+import { SettingsScreen } from "./components/SettingsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +15,22 @@ export default function App() {
     <ApplicationProvider {...eva} theme={eva.light}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <MaterialIcons
+                  name="settings"
+                  size={24}
+                  color="black"
+                  onPress={() => navigation.navigate("Settings")}
+                />
+              ),
+            })}
+          />
           <Stack.Screen name="Photo Detail" component={PhotoDetailScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </ApplicationProvider>
